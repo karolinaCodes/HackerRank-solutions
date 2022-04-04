4b7c5bf1f06381fac8b23d685f757e43b25060e7
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-04-04 12:52:36 -0400

    add solution to sales-by-match

--- 11-sales-by-match.js
+++ 11-sales-by-match.js
@@ -1,13 +1,15 @@
 function sockMerchant(n, ar) {
   let pairs = 0;
-  const filtered = ar.filter(sock => {
-    return sock === ar[0];
-  });
-  const count = Math.floor(filtered.length / 2);
-  pairs += count;
-  console.log(pairs);
+  while (ar.length > 0) {
+    const first = ar[0];
+    const allFirsts = ar.filter(el => el === first);
+    pairs += Math.floor(allFirsts.length / 2);
+    const filteredOriginal = ar.filter(el => el !== first);
+    ar = filteredOriginal;
+  }
+  return pairs;
 }
 
-sockMerchant(7, [1, 2, 1, 2, 1, 3, 2]);
-
-// FINISHED?
+// DRIVER CODE
+// sockMerchant(7, [1, 2, 1, 2, 1, 3, 2]);
+// sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]);
