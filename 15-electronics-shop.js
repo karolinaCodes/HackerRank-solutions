c23c81bb8509cab347ac1f137bf657e8e2039405
Karolina Swislocki <karolinaswislocki@hotmail.com>
2022-04-05 13:42:18 -0400

    add solution to electronics-shop

--- 15-electronics-shop.js
+++ 15-electronics-shop.js
@@ -1,14 +1,15 @@
 function getMoneySpent(keyboards, drives, b) {
-  const filtered = keyboards.filter(keyboard => keyboard !== b);
-  const mostExpenKeyboard = Math.max(...filtered);
-  let output = -1;
-  for (let i = drives.length - 1; i >= 0; i--) {
-    if (mostExpenKeyboard + drives[i] < 60) {
-      output = mostExpenKeyboard + drives[i];
-      break;
+  const max = -1;
+  for (let i = 0; i < keyboards.length; i++) {
+    let temp = 0;
+    for (let j = 0; j < drives.length; j++) {
+      if (keyboards[i] + drives[j] <= b) {
+        temp = keyboards[i] + drives[j];
+        max = temp > max ? temp : max;
+      }
     }
   }
-  return output;
+  return max;
 }
 
 const b = 60;
@@ -18,6 +19,8 @@ const drives = [5, 8, 12];
 // const result = getMoneySpent(keyboards, drives, b);
 // const result2 = getMoneySpent([3, 1], [5, 2, 8], 10);
 // 9
+const result3 = getMoneySpent([4], [5], 5);
 
 // console.log(result);
 // console.log(result2);
+console.log(result3);
